abffd7bb313fcc280cc5b584774108e78bd1d233
Wes Reed <reedws@ucmail.uc.edu>
2026-03-11 20:32:16 -0400

    added scouting page

--- src/js/hash-routing.js
+++ src/js/hash-routing.js
@@ -1,7 +1,7 @@
 const sectionBtns = document.querySelectorAll(".section-btn");
 const sections = document.querySelectorAll(".section");
 
-const sectionOrder = ["home", "robot", "about", "fll", "pgsc", "impact", "sponsors"];
+const sectionOrder = ["home", "robot", "about", "scouting", "fll", "pgsc", "impact", "sponsors"];
 
 // Make switchSection globally available
 window.switchSection = (sectionName) => {
